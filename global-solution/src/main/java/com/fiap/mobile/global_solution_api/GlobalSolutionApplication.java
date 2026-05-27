38e73e7efb17791e23d1c56bd92c1a6e65f96569
Lusca04 <rodriguwalusca@gmail.com>
2026-05-27 20:15:04 -0300

    Feat: criando spring boot estrutura e configurando h2 database

---
+++ global-solution/src/main/java/com/fiap/mobile/global_solution_api/GlobalSolutionApplication.java
@@ -0,0 +1,12 @@
+package com.fiap.mobile.global_solution_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalSolutionApplication.class, args);
+    }
+}
---
+++ global-solution/src/test/java/com/fiap/mobile/global_solution_api/GlobalSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mobile.global_solution_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
